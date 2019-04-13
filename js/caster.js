a37265532e8513f80b86704ecefd2b00b46aa228
plazma <clem-roux@hotmail.fr>
2019-04-14 00:35:49 +0200

    Removed all console.logs and added few commentaries.

--- js/caster.js
+++ js/caster.js
@@ -10,35 +10,27 @@ var currentchar = new caster();
 //Update Caster Object every time something is selected;
 document.addEventListener("nameselected", function(e) {
     currentchar.name = e.detail.name;
-    console.log(currentchar);
 });
 
 document.addEventListener("factionselected", function(e) {
     currentchar.faction = e.detail.faction;
     currentchar.basetype=undefined;
-    console.log(currentchar);
 });
 
 document.addEventListener("basetypeselected", function(e) {
     currentchar.basetype = e.detail.basetype;
-    console.log(currentchar);
 });
     
-    
+//update descriptor everytime something is selected.
 var descriptorUpdate = function(){
     var descriptor = document.querySelector(".descriptor");
-    console.log("plop31");
     if ((currentchar.name)&&(currentchar.faction)&&(currentchar.basetype)){
-        console.log("plop1");
         descriptor.innerHTML = "Your mighty new "+currentchar.basetype+" "+currentchar.faction+" warcaster's name is "+currentchar.name;
     }else if ((currentchar.name)&&(currentchar.faction)&&(!currentchar.basetype)){
-        console.log("plop2");
         descriptor.innerHTML = "Your mighty new "+currentchar.faction+" warcaster's name is "+currentchar.name;
     }else if ((!currentchar.name)&&(currentchar.faction)&&(currentchar.basetype)){
-        console.log("plop3");
         descriptor.innerHTML = "Your mighty new "+currentchar.basetype+" "+currentchar.faction+" warcaster is nameless.";
     }else if ((!currentchar.name)&&(currentchar.faction)&&(!currentchar.basetype)){
-        console.log("plop4");
         descriptor.innerHTML = "Your mighty new "+currentchar.faction+" warcaster is nameless.";
     } 
 }
