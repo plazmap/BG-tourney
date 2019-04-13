a1f3501dee3885ba1a1d916a926cdbe54cf79aac
plazma <clem-roux@hotmail.fr>
2019-04-13 21:37:40 +0200

    Changed variable names, created Data.js file, created second selector

---
+++ data.js
@@ -0,0 +1,16 @@
+//Pseudo config. Modifiable.
+
+var dataFactions = [
+    "Cryx",
+    "Cygnar",
+    "Khador",
+    "Menoth",
+    "Skorne",
+    "Other deviants"
+  ];
+  
+  var dataCharTypes = {
+    Cryx: ["Pirate", "Lich", "Sorcerer"],
+    Cygnar: ["Arcanist", "Combattant", "Gun Mage"],
+    Khador: ["Koldun", "ManHunter", "Manowar"]
+  };
--- scripts.js
+++ scripts.js
@@ -1,41 +1,63 @@
-//Pseudo config. Modifiable.
-var charFactions = ["Cryx", "Cygnar", "Khador", "Menoth", "Skorne", "Other deviants"];
-
-var charTypes ={
-    Cryx: ["Pirate", "Lich", "Sorcerer"],
-    Cygnar: ["Arcanist", "Combattant", "Gun Mage"],
-    Khador: ["Koldun", "ManHunter", "Manowar"]
-}
-
-
 //Constructor for Casters.
 function caster(name, faction, baseprofile) {
-    this.name = name;
-    this.faction = faction;
-    this.baseprofile = baseprofile;
+  this.name = name;
+  this.faction = faction;
+  this.baseprofile = baseprofile;
 }
 
-//Create faction Select's options
+//Create faction Select's options.
 
-var factions = document.querySelector("#factions");
-charFactions.forEach(charFaction => {
-    var opt = document.createElement("option");
-    opt.appendChild( document.createTextNode(charFaction) );
-    opt.value= charFaction;
-    factions.appendChild(opt);
+var factionSelector = document.querySelector("#factionselector");
+dataFactions.forEach(dataFaction => {
+  var opt = document.createElement("option");
+  opt.appendChild(document.createTextNode(dataFaction));
+  opt.value = dataFaction;
+  factionselector.appendChild(opt);
 });
 
+//Trigger customevent when faction is selected.
 
-factions.addEventListener("change", function(e){
-    var faction = e.target.value;
-    var event = new CustomEvent("factionselected", {"faction":faction});
-    document.dispatchEvent(event);
+factionSelector.addEventListener("change", function(e) {
+  var faction = e.target.value;
+  var event = new CustomEvent("factionselected", {
+    bubbles: true,
+    detail: {
+      faction: faction
+    }
+  });
+  factionSelector.dispatchEvent(event);
 });
 
-var descriptor = document.querySelector(".descriptor");
-document.addEventListener("factionselected", function(e){
-    console.log("plop");
-    console.log(e);
-    descriptor.innerHTML = "Your mighty new caster's faction is "+e.faction;
+//Upgrade descriptor with selected faction.
+document.addEventListener("factionselected", function(e) {
+  var descriptor = document.querySelector(".descriptor");
+  descriptor.innerHTML =
+    "Your mighty new caster's faction is " + e.detail.faction;
+});
+
+//Create second selector once faction is selected.
+
+document.addEventListener("factionselected", function(e) {
+  var basetypeSelector = document.querySelector("#basetypeselector");
+
+  //First, we remove any eventual options.
+
+  while (basetypeSelector.firstChild) {
+      basetypeSelector.removeChild(basetypeSelector.firstChild);
+}
+  var faction = e.detail.faction;
+  var basetypes = dataCharTypes[faction];
+  if (!basetypes) {
+    alert(
+      "Not all data available yet ! Please select other faction. (better be Cryx");
+  } else {
+    
+    basetypes.forEach(basetype => {
+      var opt = document.createElement("option");
+      opt.appendChild(document.createTextNode(basetype));
+      opt.value = basetype;
+      basetypeSelector.appendChild(opt);
+    });
+  }
 });
 
