59d2213d59ee8faf8e3a63759c41cdaa01b766a7
plazma <clem-roux@hotmail.fr>
2019-06-21 14:31:18 +0200

    Working on selectorUpdate function

--- js/selector/selector_update.js
+++ js/selector/selector_update.js
@@ -1,4 +1,5 @@
 function selectorUpdate(){
+    deleteChildren(factionSelector);
     deleteChildren(archetypeSelector);
     deleteChildren(".spell-selector");
     deleteChildren(capacitySelector);
@@ -14,8 +15,80 @@ function selectorUpdate(){
 
     if (currentchar.decisions.name){
         nameSelector.value = currentchar.decisions.name;
+    }else{
+        return;
     }
     if (currentchar.decisions.faction){
-       
+        createFactionSelector();
+    }else{
+        return;
     }
+    if (currentchar.decisions.archetype){
+        createArchetypeSelector();
+    }else{
+        return;
+    }
+    if (currentchar.decisions.firstcapacity){
+        createFirstCapacitySelector();
+    }else{
+        return;
+    }
+    if (currentchar.decisions.firstspells){
+        spellSelectors.forEach(spellSelector => {
+            createSpellSelector(spellSelector);
+          });
+    }else{
+        return;
+    }
+    if (currentchar.decisions.pts25first){
+        createFirst25Selector();
+    }else{
+        return;
+    }
+    if (currentchar.decisions.pts25second){
+        createSecond25Selector();
+    }else{
+        return;
+    }
+    if (currentchar.decisions.firststat){
+        createStatSelector(firstStatSelector, "firststat");
+    }else{
+        return;
+    }
+    if (currentchar.decisions.pts50first){
+        createFirst50Selector();
+    }else{
+        return;
+    }
+    if (currentchar.decisions.pts50second){
+        createSecond50Selector();
+    }else{
+        return;
+    }
+    if (currentchar.decisions.secondstat){
+        createStatSelector(secondStatSelector, "secondstat");
+    }else{
+        return;
+    }
+    if (currentchar.decisions.pts75first){
+        createFirst75Selector();
+    }else{
+        return;
+    }
+    if (currentchar.decisions.pts75second){
+        createSecond75Selector();
+    }else{
+        return;
+    }
+    if (currentchar.decisions.thirdstat){
+        createStatSelector(thirdStatSelector, "thirdstat");
+    }else{
+        return;
+    }
+
 }
+
+  
+        
+      
+ 
